c32c84a3f81b86ebb50b302fcb2efe5b25df2d24
Kamil Cessak <kamil.cessak@mindconsulting.pl>
2025-11-08 12:48:42 +0100

    added displaying latest wg and bp on dashboard

--- public/src/features/dashboard/controller.js
+++ public/src/features/dashboard/controller.js
@@ -1,8 +1,17 @@
+import { latestByType } from "../measurements/repo.js";
+
 export const getTodaySummary = async () => {
+  const latestWg = await latestByType("weight", 1);
+  const latestBp = await latestByType("bp", 1);
+
+  console.log({ latestWg, latestBp });
+
   return {
     date: new Date(),
     calories: { eaten: 1000, target: 2000 },
-    lastWeight: { kg: 90, ts: Date.now() - 1000 * 60 * 60 * 24 },
-    lastBp: { sys: 120, dia: 80, ts: Date.now() - 100 * 60 * 60 * 3 },
+    lastWeight: latestWg ? { kg: latestWg[0].value, ts: latestWg[0].ts } : null,
+    lastBp: latestBp
+      ? { sys: latestBp[0].value, dia: latestBp[0].value2, ts: latestBp[0].ts }
+      : null,
   };
 };
